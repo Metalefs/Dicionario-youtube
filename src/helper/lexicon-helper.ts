8f7407a0d848887dbdf5d41a847d0c1f7e3bf727
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-10-10 22:58:27 -0300

    fix: random-word

--- src/helper/lexicon-helper.ts
+++ src/helper/lexicon-helper.ts
@@ -6,13 +6,14 @@ export function getRandomInt(min, max) {
 }
 
 export function getRandomWord(wordResult: any, word: string) {
-    const nameOrRandomRelated = [wordResult.name, wordResult.related[getRandomInt(0, wordResult?.related?.length - 1)]][getRandomInt(0, 1)]
+    const nameOrRandomRelated = [wordResult.name, wordResult.related[getRandomInt(0, wordResult?.related?.length - 1)]?.name][getRandomInt(0, 1)]
 
     const definitionOrnameOrRandomRelatedIfNoNumber = /\d/.test(wordResult?.definition) ? nameOrRandomRelated : wordResult?.definition
     const randSynonym = wordResult?.synonyms[getRandomInt(0, wordResult?.synonyms?.length - 1)]?.name
 
-    let result = [randSynonym || definitionOrnameOrRandomRelatedIfNoNumber, definitionOrnameOrRandomRelatedIfNoNumber || wordResult?.name, wordResult?.name, word][getRandomInt(0, 3)]
-    result = result.replace('.', '').replace('..', ' ')
+    let result = [randSynonym || definitionOrnameOrRandomRelatedIfNoNumber || wordResult?.name, definitionOrnameOrRandomRelatedIfNoNumber || wordResult?.name, wordResult?.name || word, word][getRandomInt(0, 3)]
+    console.log(result);
+    result = result.replace('.', '').replace('..', ' ');
 
     if (word.endsWith(',') || word.endsWith(':') || word.endsWith('.')) {
         result = result + word[word.length - 1]
