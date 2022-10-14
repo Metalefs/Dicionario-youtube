84d976435a72b1f321e4f966dc492db43ff8b852
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-10-14 01:37:53 -0300

    fix: ignore .env

--- scripts/build-csr.ts
+++ scripts/build-csr.ts
@@ -1,4 +1,4 @@
 import { execSync } from 'child_process';
 
-execSync('move .\\client\\index.html .\\', { stdio: 'inherit' });
+//execSync('move .\\client\\index.html .\\', { stdio: 'inherit' });
 execSync('vite build --outDir dist/client', { stdio: 'inherit' });
--- scripts/build-ssr.ts
+++ scripts/build-ssr.ts
@@ -3,4 +3,4 @@ import { execSync } from 'child_process';
 execSync('vite build --outDir dist/server --ssr client/entry-server.ts', {
   stdio: 'inherit',
 });
-execSync('move .\\index.html .\\client', { stdio: 'inherit' });
+//execSync('move .\\index.html .\\client', { stdio: 'inherit' });
