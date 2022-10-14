1e78847fc4e68ed7f6c789fc7af61303095defea
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-10-13 23:02:23 -0300

    feat: loading phrase definition on hover

--- client/main.ts
+++ client/main.ts
@@ -2,11 +2,15 @@ import App from './App.vue';
 import { createSSRApp } from 'vue';
 import { createRouter } from './router';
 import pinia from './stores' // importing Pinia
+import FloatingVue from 'floating-vue'
+import 'floating-vue/dist/style.css'
 
 export function createApp() {
   const app = createSSRApp(App);
   const router = createRouter();
   app.use(router);
   app.use(pinia);
+  app.use(FloatingVue)
+
   return { app, router, pinia };
 }
