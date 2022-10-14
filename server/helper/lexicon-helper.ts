76848ee1f7a288aab24992f6c35bebdbc1785099
Jackson Ramalho <jackson.pires.rm@gmail.com>
2022-10-14 11:32:06 -0300

    fix: getRandomWord

--- server/helper/lexicon-helper.ts
+++ server/helper/lexicon-helper.ts
@@ -5,7 +5,7 @@ export function getRandomInt(min, max) {
 }
 export function getRandomWord(wordResult: any, word: string) {
     const randRelatedIdx = getRandomInt(0, wordResult?.related?.length - 1 || 0);
-    const relatedOrWord = !!wordResult?.related.at ? wordResult?.related?.at(randRelatedIdx)?.name : word;
+    const relatedOrWord = !!wordResult?.related ? wordResult?.related?.at(randRelatedIdx)?.name : word;
     const nameOrRandomRelated = [wordResult?.name ?? word, relatedOrWord][getRandomInt(0, 1)]
     let randDefIdx = 0;
     try{
@@ -14,7 +14,7 @@ export function getRandomWord(wordResult: any, word: string) {
     catch(Ex){}
     const definitionOrnameOrRandomRelatedIfNoNumber = /\d/.test(wordResult?.definitions?.at(randDefIdx) ?? '') ? nameOrRandomRelated : wordResult?.definitions?.at(randDefIdx) ?? word;
     const randSynonIdx = getRandomInt(0, wordResult?.synonyms?.length - 1 || 0);
-    const randSynonymOrWord = !!wordResult?.synonyms.at ? wordResult?.synonyms?.at(randSynonIdx)?.name : word;
+    const randSynonymOrWord = !!wordResult?.synonyms?.at ? wordResult?.synonyms?.at(randSynonIdx)?.name : word;
 
     let result = [(randSynonymOrWord ?? definitionOrnameOrRandomRelatedIfNoNumber) || (wordResult?.name ?? word), definitionOrnameOrRandomRelatedIfNoNumber || (wordResult?.name ?? word), word][getRandomInt(0, 2)]
     result = result.replace('.', '').replace('..', ' ');
