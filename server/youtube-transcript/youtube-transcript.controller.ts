907c6007c9ec021351af46a618008a109a51d3f9
Jackson Ramalho <jackson.pires.rm@gmail.com>
2024-02-01 21:27:36 -0300

    bogus

--- server/youtube-transcript/youtube-transcript.controller.ts
+++ server/youtube-transcript/youtube-transcript.controller.ts
@@ -7,7 +7,7 @@ export class YoutubeTranscriptController {
   @Get('transcript/:id')
   async get(@Param() params): Promise<any> {
     try{
-      return await YoutubeTranscript.fetchTranscript(params.id)
+      return await YoutubeTranscript['fetchTranscript'](params.id)
     }
     catch(ex){
       return [{text:'[Transcription disabled]', duration: Number.MAX_SAFE_INTEGER, offset: 0, ex: JSON.stringify(ex)}]
